20c7010a8ab660042a9c23118acfd7aad1d58ea6
Jordan Cox <jordancox747@outlook.com>
2023-04-11 06:58:18 -0600

    new commit

--- Form a Story/main.js
+++ Form a Story/main.js
@@ -2,14 +2,14 @@
 const words = new URLSearchParams(window.location.search);
 
 // Cleans up and capitalizes the names of the animals
-function cleanAndCap (str){
-  if(!str) return null
+function cleanAndCap(str) {
+  if (!str) return null
   let temp = str.trim()
   return temp[0].toUpperCase() + temp.substring(1)
 }
 
 // Assigning the variables with values used in the story
-const firstAnimal= cleanAndCap(words.get('animal-1'));
+const firstAnimal = cleanAndCap(words.get('animal-1'));
 const secondAnimal = cleanAndCap(words.get('animal-2'));
 
 const answer = cleanAndCap(words.get('answer'));
